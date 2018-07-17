b92137474eac9b79fcead90ba692881037137c69
Shihab Rahman <shihab.csedu09@gmail.com>
2018-07-17 16:33:58 +0600

    Add week2 solution up to compute cost

--- week2/machine-learning-ex1/ex1/computeCost.m
+++ week2/machine-learning-ex1/ex1/computeCost.m
@@ -1,3 +1,26 @@
-t=[0:0.01:0.98]
-y1 = sin(2*pi*4*t)
-plot(t,y1);
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+
+
+h_x_theta = X*theta
+
+
+J = (1/(2*m))*sum(power((h_x_theta - y),2));
+
+
+% =========================================================================
+
+end
