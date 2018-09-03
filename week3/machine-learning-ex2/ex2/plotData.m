786335e2480842fc4b2ad7e3b69749ca9ecad3a1
Shihab Rahman <shihab.csedu09@gmail.com>
2018-09-03 14:52:42 +0600

    Add plotting of data for logistic regression

--- week3/machine-learning-ex2/ex2/plotData.m
+++ week3/machine-learning-ex2/ex2/plotData.m
@@ -1,22 +1,18 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
+%PLOTDATA Plots the data points X and y into a new figure
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
 % Create New Figure
 figure; hold on;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the positive and negative examples on a
-%               2D plot, using the option 'k+' for the positive
-%               examples and 'ko' for the negative examples.
-%
+% Get positive and negative examples indices
+positive_examples = find(y == 1);
+negative_examples = find(y == 0);
 
 
-
-
-
-
+plot(X(positive_examples,1), X(positive_examples,2), '+', 'LineWidth', 2, 'MarkerSize', 6);
+plot(X(negative_examples,1), X(negative_examples,2), 'o', 'MarkerFaceColor', 'yellow', 'MarkerSize', 6);
 
 
 
