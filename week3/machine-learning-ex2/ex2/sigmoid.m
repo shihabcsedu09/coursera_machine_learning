75a39b239530895e7cdb6a3d7a8f3a52044fbf7d
Shihab Rahman <shihab.csedu09@gmail.com>
2018-09-04 16:29:35 +0600

    Add cost and gradient

--- week3/machine-learning-ex2/ex2/sigmoid.m
+++ week3/machine-learning-ex2/ex2/sigmoid.m
@@ -1,18 +1,16 @@
-function g = sigmoid(z)
-%SIGMOID Compute sigmoid function
-%   g = SIGMOID(z) computes the sigmoid of z.
+  function g = sigmoid(z)
+  %SIGMOID Compute sigmoid function
+  %   g = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
-g = zeros(size(z));
+  % You need to return the following variables correctly
+  g = zeros(size(z));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
-%               vector or scalar).
+  for rowIter  = 1:size(z,1)
+     for colIter = 1:size(z,2)
+         numerator = 1 ;
+         denominator = 1 + exp( -z(rowIter,colIter) ) ;
+         g(rowIter,colIter) = numerator / denominator ;
+     end
+  end
 
-
-
-
-
-% =============================================================
-
-end
+  end
