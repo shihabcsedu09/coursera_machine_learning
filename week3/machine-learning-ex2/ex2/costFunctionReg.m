bede3bd2d6504e9ecee8729e9caa59532f088f43
Shihab Rahman <shihab.csedu09@gmail.com>
2018-09-09 10:18:36 +0600

    Finalized week3 assignment

--- week3/machine-learning-ex2/ex2/costFunctionReg.m
+++ week3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -1,27 +1,31 @@
-function [J, grad] = costFunctionReg(theta, X, y, lambda)
-%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
-%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
-%   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+function[J, grad] = costFunctionReg(theta, X, y, lambda)
 
-% Initialize some useful values
-m = length(y); % number of training examples
+m = length(y);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+
+h_theta = sigmoid(X * theta);
+
+
+disp((theta(2 : length(theta))'));
+disp(theta(2 : length(theta)));
+
+cost_first_part = (1 / m) * ((-y' * log(h_theta)) - (1 - y)' * log(1 - h_theta));
+cost_regularization_part = ((lambda / (2 * m)) * ((theta(2 : length(theta)))' * theta(2 : length(theta))));
+
+J = cost_first_part + cost_regularization_part ;
 
 
 
+theta(1) = 0;
+
+gradient_descent_first_part = (1 / m) * (h_theta - y)' * X;
+gradient_descent_second_part = lambda / m * theta'
 
 
-
-% =============================================================
+grad = gradient_descent_first_part + gradient_descent_second_part
 
 end
--- week3/machine-learning-ex2/ex2/ex2_reg.m
+++ week3/machine-learning-ex2/ex2/ex2_reg.m
@@ -1,41 +1,22 @@
-%% Initialization
 clear ; close all; clc
-
-%% Load Data
-%  The first two columns contains the X values and the third column
-%  contains the label (y).
 
 data = load('ex2data2.txt');
 X = data(:, [1, 2]); y = data(:, 3);
 
 plotData(X, y);
 
-% Put some labels
+
 hold on;
 
-% Labels and Legend
+
 xlabel('Microchip Test 1')
 ylabel('Microchip Test 2')
 
-% Specified in plot order
+
 legend('y = 1', 'y = 0')
 hold off;
 
 
-%% =========== Part 1: Regularized Logistic Regression ============
-%  In this part, you are given a dataset with data points that are not
-%  linearly separable. However, you would still like to use logistic
-%  regression to classify the data points.
-%
-%  To do so, you introduce more features to use -- in particular, you add
-%  polynomial features to our data matrix (similar to polynomial
-%  regression).
-%
-
-% Add Polynomial Features
-
-% Note that mapFeature also adds a column of ones for us, so the intercept
-% term is handled
 X = mapFeature(X(:,1), X(:,2));
 
 % Initialize fitting parameters
